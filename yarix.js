e3cfcc168e5d4e3e608b28ba061b175556ebe259
Yarix963 <88404810+Yarix963@users.noreply.github.com>
2021-08-17 05:29:30 -0500

    Create yarix.js

---
+++ yarix.js
@@ -0,0 +1,28 @@
+const { WAConnection } = require('@adiwajshing/baileys');
+const fs = require('fs');
+
+async function iniciar () { 
+        const client = new WAConnection()
+
+        client.logger.level = 'warn'
+
+        client.on('qr', () => {
+        })
+
+        fs.existsSync('./Samu330.json') && client.loadAuthInfo('./Samu330.json')
+
+        client.on('connecting', () => {
+        console.log('Conectando')
+        })
+
+        client.on('open', () => {
+        console.log('Conectado exitosamente :D')
+        })
+        await client.connect({timeoutMs: 30*1000})
+        fs.writeFileSync('./Samu330.json', JSON.stringify(client.base64EncodedAuthInfo(), null, '\t'))
+        }
+
+iniciar ()
+.catch (err => console.log("unexpected error: " + err))
+
+
